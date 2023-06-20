96b8aacf6f87eedd9b1b7d49fdfe947fa5013fc9
Syam Konala <syamkonala30@gmail.com>
2023-06-20 09:45:53 -0500

    Add files via upload

---
+++ Factorial Calculator.cpp
@@ -0,0 +1,44 @@
+// Syam Konala
+// BMEN 1300-002
+// 11/29/2022
+
+#include <iostream>
+using namespace std;
+
+int calculateFactorial (int);
+
+
+int main()
+{
+    int pos_int;
+    
+    cout << "Enter a positive integer: " << endl;
+    cin >> pos_int;
+    while (pos_int < 0)
+    {
+        cout << "Error. It is impossible to calculate factorial with a negative number." << endl;
+        cin >> pos_int;
+    }
+    cout << "The factorial of " << pos_int << " is " << calculateFactorial(pos_int) << "." << endl;
+    return 0;
+}
+
+int calculateFactorial(int pos_int)
+{
+    long int factorial = 1;
+    
+    if (pos_int > 1)
+    {
+        for (int i = 1; i <= pos_int; i++)
+        {
+            factorial *= i;
+        }
+        
+    }
+    else
+    {
+        return 1;
+    }
+    return factorial;
+}
+
